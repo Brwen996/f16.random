c9dbb03a8c7d3e74eaf9076049f6a82cd83e9656
F16 <73608415+f1697@users.noreply.github.com>
2021-01-23 00:38:57 +0300

    Update f16.py

--- f16.py
+++ f16.py
@@ -1,6 +1,6 @@
 import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
 os.system('rm -rf .txt')
-for n in range(100000):
+for n in range(2500):
     nmbr = random.randint(1111111, 9999999)
     sys.stdout = open('.txt', 'a')
     print nmbr
